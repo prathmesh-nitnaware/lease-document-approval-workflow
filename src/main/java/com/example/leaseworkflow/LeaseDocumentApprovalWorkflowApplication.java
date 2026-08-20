614a2bdfb5b23a7856c9ee9082d58e4d8b153e0b
Prathmesh Nitnaware <nitnaware.prathmesh@gmail.com>
2026-08-20 16:38:20 +0530

    chore: initial Spring Boot project skeleton

---
+++ src/main/java/com/example/leaseworkflow/LeaseDocumentApprovalWorkflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.leaseworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseDocumentApprovalWorkflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaseDocumentApprovalWorkflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/leaseworkflow/LeaseDocumentApprovalWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leaseworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseDocumentApprovalWorkflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
